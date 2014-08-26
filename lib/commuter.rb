908236ca20ad6cd2e15d75a14e86181dd2df5502
Ian Knox <knoxid@gmail.com>
2014-08-26 01:37:58 -0600

    formatted output

--- lib/commuter.rb
+++ lib/commuter.rb
@@ -1,10 +1,8 @@
 require "csv"
-require "pp"
 require "date"
 
 
 commuters = {}
-ordered_weekdays = Date::DAYNAMES
 
 CSV.foreach("./data/gschool_commute_data.csv", :headers => true) do |row|
   if commuters.keys.include?(row.fields[0])
@@ -16,6 +14,7 @@ CSV.foreach("./data/gschool_commute_data.csv", :headers => true) do |row|
 end
 
 # SORT HASH
+ordered_weekdays = Date::DAYNAMES
 commuters.values.each do |commuter_data|
   commuter_data.each { |commute| commute["Weekday"] = ordered_weekdays.index(commute["Day"]) }
   commuter_data.sort_by! { |commute| [commute["Week"], commute["Weekday"]] }
@@ -24,7 +23,7 @@ end
 
 def nate_wed_inbound(name, day)
   if name == "Nate" && day["Week"] == "4" && day["Day"] == "Wednesday"
-    puts "Nate's Wed Inbound: " + day["Inbound"]
+    puts "Nate's Wed Inbound: #{day["Inbound"]}"
   end
 end
 
@@ -44,7 +43,7 @@ commuters.values.each do |commutes|
   end
 end
 
-puts "Average Commute: " + (total_time.to_f / commute_count).to_s
+puts "Average Commute: #{(total_time.to_f / commute_count).to_s}"
 
 # FASTEST WALKER
 walks = []
@@ -57,7 +56,7 @@ commuters.each do |name, commutes|
 end
 
 fastest_walker = walks.uniq.sort_by! {|walk| walk.values }.first
-puts "Fastest Walker: " + fastest_walker.keys[0] + " - " + fastest_walker.values[0]
+puts "Fastest Walker: #{fastest_walker.keys[0]} - #{fastest_walker.values[0]}"
 
 speeds = []
 commuters["Emily"].each do |commute|
@@ -66,4 +65,4 @@ commuters["Emily"].each do |commute|
   end
 end
 
-puts speeds.reduce(:+)/speeds.length
+puts "Emily's average speed: #{speeds.reduce(:+)/speeds.length}"
